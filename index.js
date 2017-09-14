8cd7607e02536b10fb8dbd79f3d815175e0a78e4
Raymond Aranda <raymond.aranda@gmail.com>
2017-09-14 06:33:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ var katzDeli = [];
 var katzDeliLine = [];
 
 function takeANumber(katzDeliLine, person) {
-
-  for (var i = 0; i < katzDeliLine.length; i++) {
   katzDeliLine.push(person)
 
-  }
-  return `Welcome, ${person}. You are number ${katzDeliLine.indexOf(person)} in line.`
+  return `Welcome, ${person}. You are number ${katzDeliLine.length} in line.`
 }
